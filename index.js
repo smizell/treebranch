bac24babba51bf2970c8c21440431beb675e1f25
Stephen Mizell <smizell@gmail.com>
2018-06-27 21:23:27 -0500

    Refactor the rest of the code to use matcher

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const {
 } = require('./lib/nodes');
 
 const { Matcher } = require('./lib/utils');
+const valueChecks = require('./lib/valueChecks');
 
 
 // Create a sort of runtime with a way to register code and create a language
@@ -55,8 +56,7 @@ class TreeBranch {
       }, {});
     });
 
-    let val = matcher.match(item);
-    return val;
+    return matcher.match(item);
   }
 }
 
@@ -71,38 +71,26 @@ function createLanguage(langName, funcNames, expressionBuilder) {
 
 // Allows for converting JavaScript primitives to node types
 function handleValue(arg) {
-  if (Number.isInteger(arg)) {
-    return new NumNode(arg);
-  }
-  if (Array.isArray(arg)) {
-    return new ArrNode(arg.map(handleValue));
-  }
-  if ((typeof arg) === 'string') {
-    return new StrNode(arg);
-  }
-  if ((typeof arg) === 'boolean') {
-    return new BoolNode(arg);
-  }
-  if (arg === null) {
-    return new NullNode();
-  }
-  if (arg === undefined) {
-    return new UndefNode();
-  }
-  if (arg.constructor === Object) {
-    let pairs = Object.keys(arg).map((key) => {
+  let matcher = new Matcher();
+
+  matcher.pattern(valueChecks.isNumber, val => new NumNode(val));
+  matcher.pattern(valueChecks.isArray, val => new ArrNode(val.map(handleValue)));
+  matcher.pattern(valueChecks.isString, val => new StrNode(val));
+  matcher.pattern(valueChecks.isBoolean, val => new BoolNode(val));
+  matcher.pattern(valueChecks.isNull, _ => new NullNode());
+  matcher.pattern(valueChecks.isUndefined, _ => new UndefNode());
+  matcher.pattern(valueChecks.isFunction, val => new FuncNode(val));
+  matcher.onNode(ExprNode, val => val);
+
+  matcher.pattern(valueChecks.isObject, (val) => {
+    let pairs = Object.keys(val).map((key) => {
       return [handleValue(key), handleValue(arg[key])];
     })
     return new ObjNode(pairs)
-  }
-  if ((typeof arg) === 'function') {
-    return new FuncNode(arg);
-  }
-  if (arg instanceof ExprNode) {
-    return arg
-  }
+  });
 
-  throw TypeError(`Cannot handle arg ${arg}`)
+  return matcher.match(arg);
+
 }
 
 // Used for creating the language. It takes arguments and converts them to
@@ -119,38 +107,27 @@ let serializers = {
 }
 
 function toList(item) {
-  // Expressions are namespaced with the language name
-  if (item instanceof ExprNode) {
-    return [`${item.info.langName}/${item.name}`].concat(item.args.map(toList));
-  }
-  if (item instanceof NumNode) {
-    return ['number', item.value];
-  }
-  if (item instanceof StrNode) {
-    return ['string', item.value];
-  }
-  if (item instanceof BoolNode) {
-    return ['boolean', item.value];
-  }
-  if (item instanceof NullNode) {
-    return ['null'];
-  }
-  if (item instanceof UndefNode) {
-    return ['undefined'];
-  }
-  if (item instanceof ArrNode) {
-    return ['array'].concat(item.items.map(toList));
-  }
-  if (item instanceof ObjNode) {
-    return ['object'].concat(item.pairs.map((item) => {
+  let matcher = new Matcher();
+
+  matcher.onNode(ExprNode, (exprNode) => {
+    return [`${exprNode.info.langName}/${exprNode.name}`].concat(exprNode.args.map(toList));
+  });
+
+  matcher.onNode(NumNode, numNode => ['number', numNode.value]);
+  matcher.onNode(StrNode, strNode => ['string', strNode.value]);
+  matcher.onNode(BoolNode, boolNode => ['boolean', boolNode.value]);
+  matcher.onNode(NullNode, _ => ['null']);
+  matcher.onNode(UndefNode, _ => ['undefined']);
+  matcher.onNode(ArrNode, arrNode => ['array'].concat(arrNode.items.map(toList)));
+  matcher.onNode(FuncNode, _ => ['native-function']);
+
+  matcher.onNode(ObjNode, (objNode) => {
+    return ['object'].concat(objNode.pairs.map((item) => {
       return [toList(item[0]), toList(item[1])];
     }));
-  }
-  // Native functions cannot be serialized
-  if (item instanceof FuncNode) {
-    return ['native-function']
-  }
+  });
 
+  return matcher.match(item);
 }
 
 module.exports = {
